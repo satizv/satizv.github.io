315415e8d4e4d90a832607d1b6bcd5d273b81f39
satish <satizv@gmail.com>
2017-07-21 17:07:01 -0500

    fullpage

---
+++ pg3svg.js
@@ -0,0 +1,17 @@
+var svg = d3.select("#pg3svg")
+			.append("svg")
+			.attr("width", 640)
+			.attr("height", 500)
+			.attr("class", "zindex");
+
+    margin = {top: 40, right: 20, bottom: 35, left: 80},
+    width = 540 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var h = svg.append("g").attr("transform", "translate(" + margin.left + "," + 0 + ")");
+    h.append("g")
+    .attr("transform", "translate(145,20)")
+    .append ("text")
+    .text("Shark Tank - Overview")
+    .attr("class", "hgraph");
+
