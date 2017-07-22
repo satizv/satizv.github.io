57bb050653ce46f2fa8fc36b41f4e8e9d96ae208
satish <satizv@gmail.com>
2017-07-22 09:15:36 -0500

    fullpage

--- pg3svg.js
+++ pg3svg.js
@@ -89,8 +89,57 @@ function runpg3()
 
 function drawpg3graph(data)
 {
+
+  g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var y = d3.scaleBand()
+    .rangeRound([0, height])
+    .paddingInner(0.05)
+    .align(0.1);
+
+  var x = d3.scaleLinear()
+    .rangeRound([0,width - 100]);
+
+  var z = d3.scaleOrdinal()
+    .range(["#4A8393","#4A8393","#4A8393", "#ACC7D1","#ACC7D1","#ACC7D1","#ACC7D1","#ACC7D1","#ACC7D1","#ACC7D1"]);
+
+  var tooltip = d3.select("#tooltip");
+
+  console.log(data.map(function(d) { return d.Category; }));
+  console.log(data.map(function(d) { return d.Closed; }));
+  console.log(data.map(function(d) { return +d.Total; }));
+
+
+  console.log("max");
+  console.log(d3.max(data, function(d) { return d.Total; }));
+  y.domain(data.map(function(d) { return d.Category; }));
+  x.domain([0, d3.max(data, function(d) { return +d.Total; })]).nice();
+
+  console.log(z("Food"));
+
+  var values = data.map(function(d) { return d.Total; })
+  console.log(values);  
+  console.log(values[0]);
+
+ 
+
+  g.append("g")
+    .attr("transform", "translate(150,355)")
+    .append ("text")
+    .text("Number of Deals")
+    .attr("class", "axistext");
+
+  g.append("g")
+      .attr("class", "axis")
+      .call(d3.axisLeft(y).ticks(null, "s"));
+  
+  g.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
     var keys = data.map(function(d) { return d.Category; });
-    
+
     g.append("g")
     .selectAll("g")
     .data(d3.stack().keys(keys)(data))
