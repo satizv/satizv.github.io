5a8b8ed0e7ea17bbe6d946a6b4e502a43349cdc7
satish <satizv@gmail.com>
2017-07-23 16:38:56 -0500

    Initial commit

--- pg3svg.js
+++ pg3svg.js
@@ -280,6 +280,7 @@ function drawpg3graph(data)
     g.append("g")
     .attr("transform", "translate(450,280)")
     .append ("path")
+    .datum({endAngle: 0.127 * tau})
     .attr("d", arc)
     .attr("id","pg3svgarc1")
     .attr("class", "arc");
@@ -287,7 +288,6 @@ function drawpg3graph(data)
     values.forEach(function(d,i) {
     var del = i*2000+2000;
     d3.select("#pg3svgarc1")
-    .datum({endAngle: 0.127 * tau})
     .transition()
     .delay(del)
     .duration(250)
