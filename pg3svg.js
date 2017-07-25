3fb14bbd4c563473cc98081fb8aefce3a0e393e9
satish <satizv@gmail.com>
2017-07-25 09:48:05 -0500

    Initial commit

--- pg3svg.js
+++ pg3svg.js
@@ -138,22 +138,17 @@ function drawpg3graph(data)
     .append ("text")
     .text("Number of Deals")
     .attr("class", "axistext");
-/*
+
   g.append("g")
     .attr("transform", "translate(10,10)")
     .append ("text")
     .text("Category")
     .attr("class", "axistext");
-*/
+
   g.append("g")
       .attr("class", "axis")
       .call(d3.axisLeft(y).ticks(null, "s"))
-        .append("text")
-        .text("Category")
-        .attr("class", "axistext")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -50)
-        .attr("y", -40);
+      ;
   
   g.append("g")
     .attr("class", "axis")
