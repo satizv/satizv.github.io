c7f20cbee1e99252490ece9dc19c8445a001bb20
satish <satizv@gmail.com>
2017-07-24 22:20:02 -0500

    Initial commit

--- pg3svg - Copy (4).js
+++ pg3svg - Copy (4).js
@@ -64,8 +64,8 @@ function removegraph()
 function drawpg3graph(data)
 {
 
-  var pg3tooltip = d3.select("#pg3tooltip");
-  var y = d3.scaleBand()
+     var pg3tooltip = d3.select("#pg3tooltip");
+   var y = d3.scaleBand()
     .rangeRound([0, height])
     .paddingInner(0.05)
     .align(0.1);
@@ -126,9 +126,9 @@ function drawpg3graph(data)
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
-  var keys = data.map(function(d) { return d.Category; });
+    var keys = data.map(function(d) { return d.Category; });
 
-  g.append("g")
+    g.append("g")
     .selectAll("g")
     .data(d3.stack().keys(keys)(data))
     .enter().append("g")
@@ -158,7 +158,7 @@ function drawpg3graph(data)
       ;
 
 
-  g.append("g")
+    g.append("g")
     .selectAll("g")
     .data(d3.stack().keys(keys)(data))
     .enter().append("g")
@@ -188,19 +188,19 @@ function drawpg3graph(data)
       ;
 
 
-  var totClosed = d3.sum(data, function(d) { return d.Closed; });
-  console.log(totClosed);
+    var totClosed = d3.sum(data, function(d) { return d.Closed; });
+    console.log(totClosed);
 
-  var totTotal = d3.sum(data, function(d) { return d.Total; });
+    var totTotal = d3.sum(data, function(d) { return d.Total; });
  //   var totTotal = [770,1000];
-  console.log(totTotal);
+    console.log(totTotal);
 
-  var formpcnt = d3.format(".0%");   
+    var formpcnt = d3.format(".0%");   
 
-  var easement = d3.easeCubic;
-  var format = d3.format(",d");
+    var easement = d3.easeCubic;
+    var format = d3.format(",d");
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,20)")
     .append ("text")
     .text("Category")
@@ -208,7 +208,7 @@ function drawpg3graph(data)
     .attr("alignment-baseline","central")
     .attr("class", "sidebar");
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,40)")
     .append ("text")
     .text("All")
@@ -218,7 +218,7 @@ function drawpg3graph(data)
     .attr("id","pg3svgtxtcat");    
 
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,68)")
     .append ("text")
     .attr("text-anchor","middle")
@@ -226,7 +226,7 @@ function drawpg3graph(data)
     .attr("class", "hsidebar")
     .attr("id","pg3svgtxt1");
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,95)")
     .append ("text")
     .text("Total Presented")
@@ -234,7 +234,7 @@ function drawpg3graph(data)
     .attr("alignment-baseline","central")
     .attr("class", "sidebar");
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,115)")
     .append ("text")
     .text("in 8 seasons.")
@@ -242,7 +242,7 @@ function drawpg3graph(data)
     .attr("alignment-baseline","central")
     .attr("class", "sidebar");
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,145)")
     .append ("text")
     .attr("id","pg3svgtxt2")
@@ -250,7 +250,7 @@ function drawpg3graph(data)
     .attr("alignment-baseline","central")
     .attr("class", "hsidebar");
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,172)")
     .append ("text")
     .text("Deals closed in")
@@ -258,7 +258,7 @@ function drawpg3graph(data)
     .attr("alignment-baseline","central")
     .attr("class", "sidebar");
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,192)")
     .append ("text")
     .text("8 seasons.")
@@ -266,7 +266,7 @@ function drawpg3graph(data)
     .attr("alignment-baseline","central")
     .attr("class", "sidebar");
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,280)")
     .append ("text")
     .text("0")
@@ -285,7 +285,7 @@ function drawpg3graph(data)
           });
 
 
-  values.forEach(function(d,i) {
+    values.forEach(function(d,i) {
     var del = i*1000+1000;
     d3.select("#pg3svgtxt1")
     .transition()
@@ -297,9 +297,9 @@ function drawpg3graph(data)
             var x = d3.interpolateNumber(that.text(),(a + +that.text()));
             return function(t) { that.text(format(x(t))); };
           });
-  });
+});
 
-  valuesClosed.forEach(function(d,i) {
+    valuesClosed.forEach(function(d,i) {
     var del = i*1000+2000;
     d3.select("#pg3svgtxt2")
     .transition()
@@ -311,7 +311,7 @@ function drawpg3graph(data)
             var x = d3.interpolateNumber(that.text(),(a + +that.text()));
             return function(t) { that.text(format(x(t))); };
           });
-  });
+});
 
   var arc =  d3.arc()
             .innerRadius(30)
@@ -322,7 +322,7 @@ function drawpg3graph(data)
   
   var tau = 2 * 3.141592653589793;
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,280)")
     .append ("path")
     .datum({endAngle: 0})
@@ -330,7 +330,7 @@ function drawpg3graph(data)
     .attr("id","pg3svgarc1")
     .attr("class", "arc");
 
-  values.forEach(function(d,i) {
+    values.forEach(function(d,i) {
     var del = i*1000+1000;
     d3.select("#pg3svgarc1")
     .transition()
@@ -341,7 +341,7 @@ function drawpg3graph(data)
 
 
 
-  g.append("g")
+    g.append("g")
     .attr("transform", "translate(550,280)")
     .append ("path")
     .datum({endAngle: 0})
@@ -349,7 +349,7 @@ function drawpg3graph(data)
     .attr("id","pg3svgarc2")
     .attr("class", "darc");
 
-  valuesClosed.forEach(function(d,i) {
+    valuesClosed.forEach(function(d,i) {
     var del = i*1000+2000;
     d3.select("#pg3svgarc2")
     .transition()
@@ -357,307 +357,6 @@ function drawpg3graph(data)
     .duration(250)
     .attrTween("d", arcTween1((d/totTotal)*tau))
   });
-
-} 
-
-function drawpg3pcntgraph(data)
-{
-
-  var pg3tooltip = d3.select("#pg3tooltip");
-  var y = d3.scaleBand()
-    .rangeRound([0, height])
-    .paddingInner(0.05)
-    .align(0.1);
-
-  var x = d3.scaleLinear()
-    .rangeRound([0,width - 100]);
-
-
-  var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  y.domain(data.map(function(d) { return d.Category; }));
-  
-  x.domain([0, d3.max(data, function(d) { return +d.PcntTotal; })]).nice();
-  
-  var y = d3.scaleBand()
-    .rangeRound([0, height])
-    .paddingInner(0.05)
-    .align(0.1);
-
-  var x = d3.scaleLinear()
-    .rangeRound([0,width - 100]);
-
-  var z = d3.scaleOrdinal()
-    .range(["#4A8393","#4A8393","#4A8393", "#ACC7D1","#ACC7D1","#ACC7D1","#ACC7D1","#ACC7D1","#ACC7D1","#ACC7D1"]);
-
-  var pg3tooltip = d3.select("#pg3tooltip");
-
-  console.log(data.map(function(d) { return d.Category; }));
-  console.log(data.map(function(d) { return d.Closed; }));
-  console.log(data.map(function(d) { return +d.Total; }));
-
-
-  console.log("max");
-  console.log(d3.max(data, function(d) { return +d.PcntTotal; }));
-  y.domain(data.map(function(d) { return d.Category; }));
-  x.domain([0, d3.max(data, function(d) { return +d.PcntTotal; })]).nice();
-
-  console.log(z("Food"));
-
-  var values = data.map(function(d) { return d.Total; });
-  var valuesClosed = data.map(function(d) { return d.Closed; })
-  console.log(values);  
-  console.log(values[0]);
-
-
-  g.append("g")
-    .attr("transform", "translate(150,355)")
-    .append ("text")
-    .text("Number of Deals")
-    .attr("class", "axistext");
-
-  g.append("g")
-      .attr("class", "axis")
-      .call(d3.axisLeft(y).ticks(null, "s"));
-  
-  g.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  var keys = data.map(function(d) { return d.Category; });
-
-  g.append("g")
-    .selectAll("g")
-    .data(d3.stack().keys(keys)(data))
-    .enter().append("g")
-    //  .attr("fill", function(d) { console.log(z(d.key)); console.log(z(d.Category)); return z(d.Category); })
-    .selectAll("rect")
-    .data(function(d) { return d; })
-    .enter().append("rect")
-      //.attr("x", function(d) { return x(d.data.season); })
-      .attr("fill", "#ACC7D1")
-      .attr("x",0)
-      .attr("y", function(d) { console.log(d.data.Category); console.log(y(d.data.Category)); return y(d.data.Category); })
-      .attr("width", 0)
-      .attr("height", y.bandwidth())
-      .on('mouseover', function() { console.log('mouseover'); })
-      .on('mouseout', function() { console.log('mouseout'); })
-      .on("mouseover", function(d,i) {
-        pg3tooltip.style("opacity", 1)
-               .style("left",(d3.event.pageX)+"px")
-               .style("top",(d3.event.pageY)+"px")
-               .html("Presented - " + d.data.PcntTotal + "%");
-      })
-      .on("mouseout", function() { pg3tooltip.style("opacity", 0) })
-      .on("click", function (d) {pg3drawsidebar(d.data.Category,d.data.Total,d.data.Closed,d.data.PcntClosed);})
-      .transition()
-      .delay(function(d,i) {return (i*1000+1000)})
-      .attr("width", function(d) { console.log(d.data.PcntTotal); console.log(x(d.data.PcntTotal)); return x(d.data.PcntTotal); })
-      ;
-
-
-  g.append("g")
-    .selectAll("g")
-    .data(d3.stack().keys(keys)(data))
-    .enter().append("g")
-    //  .attr("fill", function(d) { console.log(z(d.key)); console.log(z(d.Category)); return z(d.Category); })
-    .selectAll("rect")
-    .data(function(d) { return d; })
-    .enter().append("rect")
-      //.attr("x", function(d) { return x(d.data.season); })
-      .attr("fill", "#4A8393")
-      .attr("x",0)
-      .attr("y", function(d) { console.log(d.data.Category); console.log(y(d.data.Category)); return y(d.data.Category); })
-      .attr("width", 0)
-      .attr("height", y.bandwidth())
-      .on('mouseover', function() { console.log('mouseover'); })
-      .on('mouseout', function() { console.log('mouseout'); })
-      .on("mouseover", function(d,i) {
-        pg3tooltip.style("opacity", 1)
-               .style("left",(d3.event.pageX)+"px")
-               .style("top",(d3.event.pageY)+"px")
-               .html("Closed - " + d.data.PcntClosed + "%");
-      })
-      .on("mouseout", function() { pg3tooltip.style("opacity", 0) })
-      .on("click", function (d) {pg3drawsidebar(d.data.Category,d.data.Total,d.data.Closed,d.data.PcntClosed);})
-      .transition()
-      .delay(function(d,i) {return (i*1000+2000)})
-      .attr("width", function(d) { console.log(d.data.PcntClosed); console.log(x(d.data.PcntClosed)); return x(d.data.PcntClosed); })
-      ;
-
-
-  var totClosed = d3.sum(data, function(d) { return d.Closed; });
-  console.log(totClosed);
-
-  var totTotal = d3.sum(data, function(d) { return d.Total; });
- //   var totTotal = [770,1000];
-  console.log(totTotal);
-
-  var formpcnt = d3.format(".0%");   
-
-  var easement = d3.easeCubic;
-  var format = d3.format(",d");
-
-  g.append("g")
-    .attr("transform", "translate(550,20)")
-    .append ("text")
-    .text("Category")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("class", "sidebar");
-
-  g.append("g")
-    .attr("transform", "translate(550,40)")
-    .append ("text")
-    .text("All")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("class", "sidebar")
-    .attr("id","pg3svgtxtcat");    
-
-
-  g.append("g")
-    .attr("transform", "translate(550,68)")
-    .append ("text")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("class", "hsidebar")
-    .attr("id","pg3svgtxt1");
-
-  g.append("g")
-    .attr("transform", "translate(550,95)")
-    .append ("text")
-    .text("Total Presented")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("class", "sidebar");
-
-  g.append("g")
-    .attr("transform", "translate(550,115)")
-    .append ("text")
-    .text("in 8 seasons.")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("class", "sidebar");
-
-  g.append("g")
-    .attr("transform", "translate(550,145)")
-    .append ("text")
-    .attr("id","pg3svgtxt2")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("class", "hsidebar");
-
-  g.append("g")
-    .attr("transform", "translate(550,172)")
-    .append ("text")
-    .text("Deals closed in")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("class", "sidebar");
-
-  g.append("g")
-    .attr("transform", "translate(550,192)")
-    .append ("text")
-    .text("8 seasons.")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("class", "sidebar");
-
-  g.append("g")
-    .attr("transform", "translate(550,280)")
-    .append ("text")
-    .text("0")
-    .attr("text-anchor","middle")
-    .attr("alignment-baseline","central")
-    .attr("id","pg3svgtxt3")
-    .attr("class", "hsidebar")
-    .transition()
-    .delay(2000)
-    .duration(9500)
-    .tween("text", function() {
-            var that = d3.select(this);
-            var a = +totClosed/+totTotal;
-            var x = d3.interpolateNumber(0,a);
-            return function(t) { that.text(formpcnt(x(t))); };
-          });
-
-
-  values.forEach(function(d,i) {
-    var del = i*1000+1000;
-    d3.select("#pg3svgtxt1")
-    .transition()
-    .delay(del)
-    .duration(250)
-    .tween("text", function() {
-            var that = d3.select(this);
-            var a = +d;
-            var x = d3.interpolateNumber(that.text(),(a + +that.text()));
-            return function(t) { that.text(format(x(t))); };
-          });
-  });
-
-  valuesClosed.forEach(function(d,i) {
-    var del = i*1000+2000;
-    d3.select("#pg3svgtxt2")
-    .transition()
-    .delay(del)
-    .duration(250)
-    .tween("text", function() {
-            var that = d3.select(this);
-            var a = +d;
-            var x = d3.interpolateNumber(that.text(),(a + +that.text()));
-            return function(t) { that.text(format(x(t))); };
-          });
-  });
-
-  var arc =  d3.arc()
-            .innerRadius(30)
-            .outerRadius(50)
-            .startAngle(0)
-//            .endAngle(2)
-            ;
-  
-  var tau = 2 * 3.141592653589793;
-
-  g.append("g")
-    .attr("transform", "translate(550,280)")
-    .append ("path")
-    .datum({endAngle: 0})
-    .attr("d", arc)
-    .attr("id","pg3svgarc1")
-    .attr("class", "arc");
-
-  values.forEach(function(d,i) {
-    var del = i*1000+1000;
-    d3.select("#pg3svgarc1")
-    .transition()
-    .delay(del)
-    .duration(250)
-    .attrTween("d", arcTween((d/totTotal)*tau))
-  });
-
-
-
-  g.append("g")
-    .attr("transform", "translate(550,280)")
-    .append ("path")
-    .datum({endAngle: 0})
-    .attr("d", arc)
-    .attr("id","pg3svgarc2")
-    .attr("class", "darc");
-
-  valuesClosed.forEach(function(d,i) {
-    var del = i*1000+2000;
-    d3.select("#pg3svgarc2")
-    .transition()
-    .delay(del)
-    .duration(250)
-    .attrTween("d", arcTween1((d/totTotal)*tau))
-  });
-
-} 
 
 function arcTween (newAngle) {
   return function(d) {
@@ -684,6 +383,8 @@ function arcTween1 (newAngle) {
       };
   };
 }
+
+} 
 
 function pg3drawsidebar(cat,tot,clo,pct) {
 
