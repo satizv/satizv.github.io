3569a626af8e60fcb8fdfd665d923a07d71785e6
satish <satizv@gmail.com>
2017-07-26 20:03:42 -0500

    Initial commit

--- pg3svg.js
+++ pg3svg.js
@@ -805,31 +805,31 @@ function drawpg3shgraph(data) {
 
       g.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(0," + height + ")")
+    //.attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x1).tickValues([0, 15, 30]));
   
         g.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(81," + height + ")")
+    //.attr("transform", "translate(81," + height + ")")
     .call(d3.axisBottom(x2).tickValues([0, 15, 30]));
         g.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(161," + height + ")")
+    //.attr("transform", "translate(161," + height + ")")
     .call(d3.axisBottom(x3).tickValues([0, 15, 30]));
 
         g.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(241," + height + ")")
+    //.attr("transform", "translate(241," + height + ")")
     .call(d3.axisBottom(x4).tickValues([0, 15, 30]));
 
           g.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(321," + height + ")")
+    //.attr("transform", "translate(321," + height + ")")
     .call(d3.axisBottom(x5).tickValues([0, 15, 30]));
 
               g.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(401," + height + ")")
+    //.attr("transform", "translate(401," + height + ")")
     .call(d3.axisBottom(x6).tickValues([0, 15, 30]));
 
     console.log(x1(30));
